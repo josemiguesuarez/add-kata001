a44f28842b04b83300fa7132dca6d94792fea0ba
José Miguel <jm.suarez201@uniandes.edu.co>
2016-09-21 19:50:19 -0500

    (azul) se cambia la implementación del test

--- NumerosTest.py
+++ NumerosTest.py
@@ -2,5 +2,4 @@ from unittest import TestCase
 from Numeros import Numeros
 
 class NumerosTest(TestCase):
-    def test_crearNumeros(self):
-        self.numeros = Numeros()
+    numeros = Numeros()
