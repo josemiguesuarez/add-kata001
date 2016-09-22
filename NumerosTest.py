4c833811c2a196b8eb06c38d415ddf2664466bb4
José Miguel <jm.suarez201@uniandes.edu.co>
2016-09-21 20:07:24 -0500

    (rojo) test para devolver el número de elementos con cadena vacia

--- NumerosTest.py
+++ NumerosTest.py
@@ -7,3 +7,7 @@ class NumerosTest(TestCase):
     def test_definirCadena (self):
         self.numeros.definirCadena("1,2,3,4,5")
         self.assertEqual(self.numeros.cadena, "1,2,3,4,5", "No definio la cadena")
+
+    def test_arregloConNumeroElementos (self):
+        self.numeros.definirCadena("")
+        self.assertEqual(self.numeros.darArreglo()[0], 0, "No devolvio correctamente el numero de elementos")
