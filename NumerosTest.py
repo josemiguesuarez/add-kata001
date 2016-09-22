c86a3be9508fa20ce9592764f6507e02006c1877
José Miguel <jm.suarez201@uniandes.edu.co>
2016-09-21 19:58:43 -0500

    (rojo) se crea un test que verifica que se definió la cadena en la clase Numero

--- NumerosTest.py
+++ NumerosTest.py
@@ -6,3 +6,4 @@ class NumerosTest(TestCase):
 
     def test_definirCadena (self):
         self.numeros.definirCadena("1,2,3,4,5")
+        self.assertEqual(self.numeros.cadena, "1,2,3,4,5", "No definio la cadena")
