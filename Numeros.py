ce713892575e48c20b126d636a1bda2a016e9d33
José Miguel <jm.suarez201@uniandes.edu.co>
2016-09-23 16:07:32 -0500

    (verde) dar máximo de un elemento

--- Numeros.py
+++ Numeros.py
@@ -14,7 +14,12 @@ class Numeros:
 
         if(len(arregloInts) == 0):
             min2 = float('inf')
+            max2 = -float('inf')
         else:
             min2 = min(arregloInts)
+            max2 = arregloInts[0]
 
-        return [len(arregloInts), min2, -float('inf')]
+
+
+
+        return [len(arregloInts), min2, max2]
