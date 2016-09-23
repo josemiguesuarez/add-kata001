8bb6e3e357d35693367e005f9e69be0a49da1a73
José Miguel <jm.suarez201@uniandes.edu.co>
2016-09-23 16:35:50 -0500

    (azul) refactor: se quitan líneas innecesarias

--- Numeros.py
+++ Numeros.py
@@ -21,6 +21,5 @@ class Numeros:
             max2 = max(arregloInts)
             prom = float(sum(arregloInts))/len(arregloInts)
 
-        print prom
 
         return [len(arregloInts), min2, max2, prom]
