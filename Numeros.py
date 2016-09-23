d7f43e9889607c336d7d19f190530d58ccc65df5
José Miguel <jm.suarez201@uniandes.edu.co>
2016-09-23 16:24:00 -0500

    (verde) promedio de un número

--- Numeros.py
+++ Numeros.py
@@ -15,8 +15,10 @@ class Numeros:
         if(len(arregloInts) == 0):
             min2 = float('inf')
             max2 = -float('inf')
+            prom = 0
         else:
             min2 = min(arregloInts)
             max2 = max(arregloInts)
+            prom = arregloInts[0]
 
-        return [len(arregloInts), min2, max2, 0]
+        return [len(arregloInts), min2, max2, prom]
